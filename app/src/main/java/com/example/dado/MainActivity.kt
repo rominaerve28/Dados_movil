a1ed0f2d7ba64171435b213f10c54700e54c3ae0
rominaerve28 <romina2808200@gmail.com>
2022-10-09 23:36:39 -0500

    tarea

---
+++ app/src/main/java/com/example/dado/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.dado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+        rollDice()
+
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.contentDescription = diceRoll.toString()
+
+        diceImage.setImageResource(drawableResource)
+    }
+
+    class Dice (private val numSides: Int){
+        var sides = 6
+        fun roll(): Int {
+            val  randomNumber = (1..numSides).random()
+            return randomNumber
+        }
+
+    }
+}
